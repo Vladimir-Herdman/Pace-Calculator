48d0cd739f42fe0a3faa40a7247a832465921eda
Vladimir Herdman <vovaherdman@gmail.com>
2024-10-09 21:07:27 -0400

    set up formatter skeleton and main use of command line arguments

--- pace/pace.py
+++ pace/pace.py
@@ -14,29 +14,54 @@ import pace_math as pm
 import pace_gui as pg
 
 
-# class for cleaner help statements, metavar not after each option format, just end
-class CustomHelpFormatter(argparse.HelpFormatter):
+# formatter_class declaration for cleaner help statements (e.g. '-a, --at DISTANCE')
+class CleanerHelpFormat(argparse.HelpFormatter):
     def _format_action_invocation(self, action):
-        parts = []
-        parts.append(', '.join(action.option_strings))
-        if action.metavar:
-            parts.append(action.metavar)
         try:
-            return ' '.join(parts)
-        except Exception:
+            parts = []
+            parts.append(', '.join(action.option_strings))
+            if action.metavar:
+                parts.append(action.metavar)
+                return ' '.join(parts)
+            raise
+        except:  # fallback to default formatting if error
             return super()._format_action_invocation(action)
+        
+def pace_output_formater(pace, km_convert, at_distance=None, to_distances=None):
+    print("Default paces")
+    if not (at_distance and to_distances): # print out table of values from pace at different distances (assuming mile pace)
+        pass
+    elif (at_distance and not to_distances): # print out table of values, assuming pace at given distance
+        pass
+    elif (not at_distance and to_distances): # print out table of values at distances assuming mile pace
+        pass
+    else: # convert pace at distance to new distance pace (pace 4:19 --at 1600 --to 400)
+        pass
+    exit(0)
 
 def main():
 
     version = "0.1"  # major.minor.patch (huge change)(minor addition)(if patching bug fixes)
-    parser = argparse.ArgumentParser(description=(__doc__ or "").strip(), prog='pace', formatter_class=CustomHelpFormatter)
+    parser = argparse.ArgumentParser(
+        description=(__doc__ or "").strip(), prog='pace', 
+        formatter_class=CleanerHelpFormat
+    )
 
     # add options to the CLI tool
+        # returns value
     parser.add_argument(
         "-v", "--version",
         action="version", version=version
         )
     
+        # positional
+    parser.add_argument(
+        "pace",
+        help="The initial pace at distance, so 0:6:12 is 6:12 min/mile",
+        nargs="+"
+    )
+    
+        # changes value
     parser.add_argument(
         "-a", "--at",
         metavar="DISTANCE",
@@ -47,14 +72,28 @@ def main():
         metavar="DISTANCE",
         help="specify to what distance you are converting to"
     )
-    parser.add_argument(
+
+        # boolean
+    conversion_group = parser.add_argument_group("conversion")
+    conversion_group.add_argument(
         "-k", "--km",
-        help="Switch distances and paces to be in terms of kilometers",
+        help="Set distances and paces to be in terms of kilometers",
         action="store_true"
     )
 
-    # run from command line for command
-    parser.parse_args()
+    # get the values and what was passed in command line
+    args = parser.parse_args()
+
+    # Set choices and print if shorthand used
+    try:
+        args.at = args.pace[1]
+        args.to = args.pace[2:]
+    except:
+        pass
+    
+    if (len(args.pace) > 1):
+        pace_output_formater(args.pace, args.km, args.at, args.to)
+
 
 if __name__ == "__main__":
     main()
